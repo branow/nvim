b7dbddbed019e5b9451948c93609d442e6c4adae
Orest Bodnar <orest.bodnar@plexusworldwide.com>
2025-10-11 14:41:50 +0300

    feat: adjust telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,29 @@ return {
   tag = "0.1.5",
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
+    local telescope = require("telescope")
     local builtin = require("telescope.builtin")
+
+    telescope.setup({
+      defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--color=never',
+          '--no-heading',
+          '--with-filename',
+          '--line-number',
+          '--column',
+          '--smart-case',
+        },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+        live_grep = {},
+      }
+    })
+
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
   end
